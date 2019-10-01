227919c9718df8715c7085d93c108459344faf0d
johnvorhies <jrv19@zips.uakron.edu>
2019-10-01 17:35:31 -0400

    Found that each RGB channel may give different values from findThetaC. Now using a grayscale EPI for findThetaC and applying the same filter to each RGB channel.

--- fastDualFanFilterUV.m
+++ fastDualFanFilterUV.m
@@ -12,79 +12,39 @@ function filtered_image = fastDualFanFilterUV(st_uv,d)
     %
     % ASSUMES SAME SIZE FOR S AND T DIMENSIONS
 
-    [Nt,Ns,Nv,Nu] = size(st_uv);
+    [Nt,Ns,Nv,Nu,Nc] = size(st_uv);
     st_center = ceil(Nt/2);
     v_center = ceil(Nv/2);
-
-    % Uncomment if the filter output has a loss in dynamic range
-    %st_uv = double(normalizeLF(st_uv));
+    
     st_uv = double(st_uv);
     
-    EPI = squeeze(st_uv(st_center,:,v_center,:));
-    
-    [theta_c,theta_zmin,theta_zmax] = findThetaC(EPI,1024);
-    
-    angle = ceil(length(theta_c)/2);
-    [Nb,b,M,h_bp,negNorm,N,B] = DFFilterParams(d,theta_c(2),theta_zmin(2),theta_zmax(2));
+    if Nc == 1
+        EPI = squeeze(st_uv(st_center,:,v_center,:));
 
+        [theta_c,theta_zmin,theta_zmax] = findThetaC(EPI,1024);
+        %angle = ceil(length(theta_c)/2);
+        [Nb,b,M,h_bp,negNorm,N,B] = DFFilterParams(d,theta_c(2),theta_zmin(2),theta_zmax(2));
+    else
+        EPI = squeeze(st_uv(st_center,:,v_center,:,:));
+        EPI = rgb2gray(uint16(EPI));
+        
+        [theta_c,theta_zmin,theta_zmax] = findThetaC(EPI,1024);
+        [Nb,b,M,h_bp,negNorm,N,B] = DFFilterParams(d,theta_c(3),theta_zmin(3),theta_zmax(3));
+    end
+        
     % Visualization of frequency responses
     %plotFrequencyResponse(st_uv,h_bp,b,N,B);
-
-    %-------------------------Filtering along u,v-------------------------
-    % T,V subband and hyperplanar filter
-    delay = floor((M-1)/2);
-    delay1 = zeros(Nt,delay);
-    delay2 = zeros(Ns,delay);
-    st_uv_filt = zeros(Nt,Nv,Nu,Nb);
-
-    for nu = 1:Nu
-        % compensate for group delay of filter
-        tv = [squeeze(st_uv(:,st_center,:,nu)) delay1];
-        for nb = 1:Nb
-            filtered = filter(h_bp(nb,:),1,double(tv),[],2);
-            filtered(:,1:delay) = [];
-            st_uv_filt(:,:,nu,nb) = DFIIR(filtered,b(:,:,nb),negNorm);
-        end
-    end
-
-    % sum components
-    st_uv_recon1 = zeros(Nt,Nv,Nu);
-    for nu = 1:Nu
-        for nb = 1:Nb
-            st_uv_recon1(:,:,nu) = st_uv_recon1(:,:,nu)+st_uv_filt(:,:,nu,nb);
-        end
-    end
-
-    st_uv(:,st_center,:,:) = st_uv_recon1;
     
-    % Uncomment if the filter output has a loss in dynamic range
-    %st_uv = normalizeLF(st_uv);
-
-    st_uv_recon1 = []; %#ok<NASGU>
-    
-    % S,U subband and hyperplanar filter
-    st_uv_filt = zeros(Ns,Nv,Nu,Nb);
-    for nv = 1:Nv
-        % compensate for group delay of filter
-        su = [squeeze(st_uv(st_center,:,nv,:)) delay2];
-        for nb = 1:Nb
-            filtered = filter(h_bp(nb,:),1,double(su),[],2);
-            filtered(:,1:delay) = [];
-            st_uv_filt(:,nv,:,nb) = DFIIR(filtered,b(:,:,nb),negNorm);
+    filtered_image = zeros(Nv,Nu,Nc,'uint16');
+    if Nc == 1
+        filtered_image = applyFilter(st_uv,b,h_bp,negNorm,Nb,M);
+    else
+        parfor Nc = 1:3
+            st_uv_channel = squeeze(st_uv(:,:,:,:,Nc));
+            filtered_image(:,:,Nc) = applyFilter(st_uv_channel,b,h_bp,negNorm,Nb,M);
         end
     end
-
-    % sum components
-    st_uv_recon2 = zeros(Ns,Nv,Nu);
-    for nv = 1:Nv
-        for nb = 1:Nb
-            st_uv_recon2(:,nv,:) = st_uv_recon2(:,nv,:)+st_uv_filt(:,nv,:,nb);
-        end
-    end
-
-    st_uv(st_center,:,:,:) = st_uv_recon2;
-
-    filtered_image = uint16(65535*mat2gray(squeeze(st_uv(st_center,st_center,:,:))));
+   
 end
 
 
