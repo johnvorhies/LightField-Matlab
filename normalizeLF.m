5c36e40de3b76ac914cd8995f33f913dcb9ea196
johnvorhies <jrv19@zips.uakron.edu>
2020-01-06 18:53:00 -0500

    Added RGB support.

--- normalizeLF.m
+++ normalizeLF.m
@@ -1,12 +1,24 @@
 function st_uv = normalizeLF(st_uv)
     % John Vorhies, The University of Akron, Feb 2019
-    % Normalizes input light field on 16-bit grayscale range
+    % Normalizes input light field on 16-bit grayscale or RGB range
     
-    [Nt,Ns,~,~] = size(st_uv);
-
-    for ns = 1:Ns
-        for nt = 1:Nt
-            st_uv(nt,ns,:,:) = 65535*mat2gray(squeeze(st_uv(nt,ns,:,:)));
+    [Nt,Ns,~,~,Nc] = size(st_uv);
+    
+    if Nc == 1
+        for ns = 1:Ns
+            for nt = 1:Nt
+                st_uv(nt,ns,:,:) = 65535*mat2gray(squeeze(st_uv(nt,ns,:,:)));
+            end
+        end
+    else
+        for nc = 1:Nc
+            for ns = 1:Ns
+                for nt = 1:Nt
+                    st_uv(nt,ns,:,:,nc) = 65535*mat2gray(squeeze(...
+                        st_uv(nt,ns,:,:,nc)));
+                end
+            end
         end
     end
+        
 end
